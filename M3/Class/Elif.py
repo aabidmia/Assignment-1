795ae257aba89e41a155abf68e0c665806303441
aabidmia <miaaabid@outlook.com>
2025-05-28 00:26:10 +0530

    blank line removed

--- M3/Class/Elif.py
+++ M3/Class/Elif.py
@@ -4,7 +4,7 @@ operand2= int(input('Enter the second number: '))
 operator= input('Enter the operator(+,-,*,/): ')
 
 if operator == '+':
-    print('The sum of two number is ',operand1 + operand2)
+    print('\nThe sum of two number is ',operand1 + operand2)
 elif operator == '-':
     print('The diffrerence of two number is ',operand1 - operand2)
 elif operator == '*':
@@ -13,4 +13,4 @@ elif operator == '/':
     print('The quotient of two number is ',operand1 / operand2)
 else :
     print('none')
-print("\nThank You!")
+print("Thank You!")
