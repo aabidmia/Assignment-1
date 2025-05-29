bebabd1f726d4baa7d1d235036e19231f56930f7
aabidmia <miaaabid@outlook.com>
2025-05-29 23:53:27 +0530

    Assignment 3 completed

---
+++ M4/Assignment-3/assignment_3a.py
@@ -0,0 +1,5 @@
+a=1
+num=int(input('Enter the number: '))
+for i in range(1,num+1):
+    a=a*i
+print('Factorial of ',num,' is ',a)
---
+++ M4/Assignment-3/assignment_3b.py
@@ -0,0 +1,6 @@
+num=int(input('Enter a number: '))
+sqr =num ** (1/2)
+print('Square root : ',sqr)
+import math
+print('Logarithm : ', math.log(num))
+print('Sine : ',math.sin(num))
